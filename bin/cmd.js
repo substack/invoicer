34c4f9b0d2ec9748e951f872adc4d606e0b11d56
James Halliday <mail@substack.net>
2014-01-27 14:18:14 -0800

    write config to the tex and spawn pdflatex

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,7 +5,20 @@ var path = require('path');
 var mkdirp = require('mkdirp');
 var split = require('split');
 var through2 = require('through2');
-var argv = require('minimist')(process.argv.slice(2));
+var sprintf = require('sprintf');
+var spawn = require('child_process').spawn;
+var os = require('os');
+
+var argv = require('minimist')(process.argv.slice(2), {
+    alias: { r: 'rcpt' }
+});
+var outfile = argv.o;
+
+if (!outfile) return usage(1)
+if (!argv.rcpt) return usage(1)
+if (argv.h || argv.help) return usage(0);
+
+var texsrc = fs.readFileSync(__dirname + '/invoice.tex', 'utf8');
 
 var configDir = argv.c || path.join(
     process.env.HOME || process.env.USERDIR, '.config', 'invoicer'
@@ -16,7 +29,7 @@ var configFile = path.join(configDir, 'config.json');
 if (!fs.existsSync(configFile)) {
     return prompter(function (err, cfg) {
         if (err) return console.error(err);
-        fs.writeFileSync(configFile, JSON.stringify(cfg, null, 2));
+        writeConfig(cfg);
         withConfig(cfg);
     });
 }
@@ -24,6 +37,39 @@ else withConfig(require(configFile))
 
 function withConfig (cfg) {
     console.log(cfg);
+    if (cfg.id === undefined) cfg.id = 1;
+    
+    var params = {
+        id: sprintf('%05d', cfg.id ++),
+        name: cfg.name,
+        address: cfg.address.replace(/\n/g, ' \\\\\n'),
+        rcpt: argv.rcpt
+    };
+    
+    var output = texsrc.replace(
+        /(?!\\)\${([^}]+)}/g,
+        function (_, key) { return params[key] }
+    );
+    
+    var tmpdir = path.join(os.tmpdir(), 'invoicer-' + Math.random();
+    mkdirp.sync(tmpdir);
+    fs.writeFileSync(path.join(tmpdir, 'invoice.tex'), output);
+    
+    var ps = spawn('pdflatex', [ 'invoice.tex' ], { cwd: tmpdir });
+    var err = '';
+    ps.stderr.on('data', function (buf) { err += buf });
+    ps.on('exit', function (code) {
+        if (code !== 0) return console.error(err);
+        fs.createReadStream(path.join(tmpdir, 'invoice.pdf'))
+            .pipe(fs.createWriteStream(outfile))
+        ;
+    });
+    
+    writeConfig(cfg);
+}
+
+function writeConfig (cfg) {
+    fs.writeFileSync(configFile, JSON.stringify(cfg, null, 2));
 }
 
 function prompter (cb) {
@@ -44,4 +90,13 @@ function prompter (cb) {
             next();
         }
     }));
+}
+
+function usage (code) {
+    var rs = fs.createReadStream(__dirname + '/usage.txt');
+    rs.pipe(process.stdout);
+    
+    rs.on('close', function () {
+        if (code !== 0) process.exit(code);
+    });
 }
