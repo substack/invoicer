f69cb85d06a6978a1b5641c5b9ee844a7f8dfa0c
James Halliday <mail@substack.net>
2014-01-27 21:51:11 -0800

    working example

--- bin/cmd.js
+++ bin/cmd.js
@@ -7,18 +7,20 @@ var split = require('split');
 var through2 = require('through2');
 var sprintf = require('sprintf');
 var spawn = require('child_process').spawn;
+var strftime = require('strftime');
 var os = require('os');
 
 var argv = require('minimist')(process.argv.slice(2), {
-    alias: { r: 'rcpt' }
+    alias: { r: 'rcpt', e: 'expense' }
 });
 var outfile = argv.o;
 
 if (!outfile) return usage(1)
 if (!argv.rcpt) return usage(1)
+if (!argv.e) return usage(1)
 if (argv.h || argv.help) return usage(0);
 
-var texsrc = fs.readFileSync(__dirname + '/invoice.tex', 'utf8');
+var texsrc = fs.readFileSync(__dirname + '/../invoice.tex', 'utf8');
 
 var configDir = argv.c || path.join(
     process.env.HOME || process.env.USERDIR, '.config', 'invoicer'
@@ -36,14 +38,60 @@ if (!fs.existsSync(configFile)) {
 else withConfig(require(configFile))
 
 function withConfig (cfg) {
-    console.log(cfg);
     if (cfg.id === undefined) cfg.id = 1;
     
+    var expenses = require(path.resolve(argv.e));
     var params = {
         id: sprintf('%05d', cfg.id ++),
         name: cfg.name,
         address: cfg.address.replace(/\n/g, ' \\\\\n'),
-        rcpt: argv.rcpt
+        email: cfg.email,
+        rcpt: argv.rcpt,
+        expenses: expenses.map(function (row) {
+            if (row.type === 'hours') {
+                var title = row.title || 'Consulting Hours';
+                return '{\\bf Date} & {\\bf ' + title + '} \\\\\n'
+                    + (row.hours || []).map(function (r) {
+                        return strftime('%F', new Date(r.date))
+                            + ' & ' + r.hours + ' \\\\\n'
+                        ;
+                    }).join('\n') + '\n'
+                ;
+            }
+            return '';
+        }),
+        totals: (function () {
+            var hours = expenses.reduce(function (acc, row) {
+                if (row.type !== 'hours') return acc;
+                return row.hours.reduce(function (h, r) {
+                    return h + r.hours;
+                }, acc);
+            }, 0);
+            
+            var rates = Object.keys(expenses.reduce(function (acc, row) {
+                if (row.type === 'hours') {
+                    acc[row.rate] = true;
+                }
+                return acc;
+            }, {}));
+            
+            var amount = expenses.reduce(function (acc, row) {
+                if (row.type === 'hours') {
+                    return row.rate * row.hours.reduce(function (h, r) {
+                        return h + r.hours;
+                    }, 0)
+                }
+                return acc;
+            }, 0) + ' ' + cfg.currency;
+            
+            return [
+                '{\\bf Total Hours} & {\\bf ' + hours + '}',
+                '{\\bf Hourly Rate} & {\\bf '
+                    + rates.join(',') + ' ' + cfg.currency + '}',
+                '{\\bf Total (' + cfg.currency + ')} & {\\bf ' + amount + '}',
+                '\\hline'
+            ].join('\\\\\n') + '\n';
+        })()
     };
     
     var output = texsrc.replace(
@@ -51,15 +99,18 @@ function withConfig (cfg) {
         function (_, key) { return params[key] }
     );
     
-    var tmpdir = path.join(os.tmpdir(), 'invoicer-' + Math.random();
+    var tmpdir = path.join(os.tmpdir(), 'invoicer-' + Math.random());
     mkdirp.sync(tmpdir);
     fs.writeFileSync(path.join(tmpdir, 'invoice.tex'), output);
     
-    var ps = spawn('pdflatex', [ 'invoice.tex' ], { cwd: tmpdir });
-    var err = '';
-    ps.stderr.on('data', function (buf) { err += buf });
+    var args = [ '-interaction=nonstopmode -halt-on-error', 'invoice.tex' ];
+    var ps = spawn('pdflatex', args, { cwd: tmpdir });
+    
+    ps.stdout.pipe(process.stdout);
+    ps.stderr.pipe(process.stderr);
+    
     ps.on('exit', function (code) {
-        if (code !== 0) return console.error(err);
+        if (code !== 0) return console.error('NONZERO EXIT CODE');
         fs.createReadStream(path.join(tmpdir, 'invoice.pdf'))
             .pipe(fs.createWriteStream(outfile))
         ;
@@ -73,7 +124,7 @@ function writeConfig (cfg) {
 }
 
 function prompter (cb) {
-    var fields = [ 'name', 'address', 'email' ];
+    var fields = [ 'name', 'address', 'email', 'currency' ];
     var cfg = {};
     console.log('Gathering configuration options.');
     console.log('Use \\n to denote line-breaks.');
@@ -83,7 +134,9 @@ function prompter (cb) {
     process.stdin.pipe(split()).pipe(through2(function (line, enc, next) {
         cfg[field] = line.toString('utf8').replace(/\\n/g, '\n');
         
-        if (fields.length === 0) return cb(null, cfg);
+        if (fields.length === 0) {
+            return cb(null, cfg);
+        }
         else {
             field = fields.shift();
             process.stdout.write('  ' + field + '> ');
